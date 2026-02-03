cfec3f1ac9776bc2114b2675c0ab625fa8c6a160
Vitor Cardoso <vitorsac13@gmail.com>
2026-02-03 11:43:49 -0300

    Fix paths

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import cors from 'cors'
+import passport from 'passport'
+import { Mongo } from './src/database/mongo.js'
+import { config } from 'dotenv'
+import authRouter from './src/auth/auth.js'
+import usersRouter from './src/routes/users.js'
+import productsRouter from './src/routes/products.js'
+import ordersRouter from './src/routes/orders.js'
+
+config()
+
+async function main () {
+    const hostname = 'localhost'
+    const port = 3000
+
+    const app = express()
+
+    const mongoConnection = await Mongo.connect({ mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME})
+    console.log(mongoConnection)
+
+    app.use(express.json())
+    app.use(cors())
+    app.use(passport.initialize())
+
+    app.get('/', (req, res) => {
+        res.send({
+            success: true,
+            statuscode: 200,
+            body: 'Welcome to ECommerce!'
+        })
+    })
+
+    app.use('/auth', authRouter)
+    app.use('/users', usersRouter)
+    app.use('/products', productsRouter)
+    app.use('/orders', ordersRouter)
+
+    app.listen(port, () => {
+        console.log(`Server running on http://${hostname}:${port}`)
+    })
+}
+
+main ()
